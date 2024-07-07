0f8e492d064f882823a8bcea6c4c2458e36f4501
Anant Singh <anantvikrams30@gmail.com>
2024-07-07 10:32:35 +0530

    Time: 2 ms (53.84%) | Memory: 7.2 MB (30.43%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numWaterBottles(int nb, int ne) {
+        int count=nb;
+      while(nb>=ne)
+      {
+        int x=nb/ne;
+        count+=x;
+        nb=nb-x*ne;
+        nb+=x;
+      }  
+      return count;
+    }
+};
