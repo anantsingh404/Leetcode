7667dacae338769eb3dcf3cc55143c4e2a07e7e8
Anant Vikram Singh <anantvikrams30@gmail.com>
2024-07-21 12:45:12 +0530

    Time: 124 ms (100.00%) | Memory: 122.3 MB (100.00%) - LeetSync

--- 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.cpp
+++ 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     long long minimumOperations(vector<int>& nums, vector<int>& target) {
-       int n = nums.size();
-        long incr = 0, decr = 0, ops = 0;
+        long long count=0;
+        long long incr=0;
+        long long decr=0;
 
-        for (int i = 0; i < n; i++) {
-            long diff = target[i] - nums[i];
-
-            if (diff > 0) {
-                if (incr < diff)
-                    ops += diff - incr;
-                incr = diff;
-                decr = 0;
-            } else if (diff < 0) {
-                if (decr < -diff)
-                    ops += -diff - decr;
-                decr = -diff;
-                incr = 0;
-            } else {
-                incr = decr = 0;
+      for(int i=0;i<nums.size();i++)
+      {
+        long long int diff=target[i]-nums[i];
+        if(diff>0)
+        {
+            if(incr<diff){
+            count+=diff-incr;
+            
             }
+            incr=diff;
+             decr=0;
         }
-
-        return ops;  
+        else if(diff<0)
+        {   if(-diff>decr){
+            count+=(-diff)-decr;
+            
+        }
+        decr=-diff;
+            incr=0;
+        }
+        else
+        {
+            incr=0;
+            decr=0;
+        }
+      }
+      return count;  
     }
 };
