89a4cbef686eacf8a1480e646ddb75623b96ffe9
Anant Vikram Singh <anantvikrams30@gmail.com>
2024-07-28 14:06:59 +0530

    Time: 1471 ms (33.33%) | Memory: 16.6 MB (16.67%) - LeetSync

---
+++ 3479-count-the-number-of-substrings-with-dominant-ones/count-the-number-of-substrings-with-dominant-ones.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int ans=0, len=s.length();
+        vector<int> prefix(len, 0);
+
+        for(int i=0 ; i<len ; i++)
+            prefix[i] += s[i]=='1' ? (i==0 ? 1 : prefix[i-1]+1) : (i==0 ? 0 : prefix[i-1]);
+        
+        for(int i=0 ; i<len ; i++) {
+            for(int j=i ; j<len ; j++) {
+                int ones = prefix[j] - (i==0 ? 0 : prefix[i-1]);
+                int zeros = (j-i+1) - ones;
+
+                if(ones >= zeros*zeros)     // dominant substring [i, j]
+                {
+                    ans++;
+                    int k = sqrt(ones);
+
+                    if(k > zeros)
+                    ans += (j+(k-zeros) >= len ? (len-j-1) : (k-zeros));
+
+                    j += (k-zeros);
+                }
+                else
+                j += (zeros*zeros - ones - 1);
+            }
+        }
+
+        return ans; 
+    }
+};
