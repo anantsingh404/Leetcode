b8754d7998e1e100ce997dea25622552d0346c10
Anant Vikram Singh <anantvikrams30@gmail.com>
2024-08-06 18:08:20 +0530

    Time: 542 ms (74.04%) | Memory: 220.1 MB (56.08%) - LeetSync

--- 3514-shortest-distance-after-road-addition-queries-ii/shortest-distance-after-road-addition-queries-ii.cpp
+++ 3514-shortest-distance-after-road-addition-queries-ii/shortest-distance-after-road-addition-queries-ii.cpp
@@ -14,35 +14,15 @@ vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
     for (const auto& itr : queries) {
         int sc = itr[0];
         int ec = itr[1];
-        
-        // Find the iterator to the first element greater than sc
         auto lb = st.lower_bound(sc + 1);
-        
-        // Remove elements from lb to the one just before ec
         while (lb != st.end() && *lb < ec) {
             auto x = lb++;
             st.erase(x);
         }
-        
-        // Add the result
         res.push_back(st.size() - 1);
     }
     
     return res;
 }
-
-int main() {
-    // Example usage
-    vector<vector<int>> queries = {{0, 2}, {1, 3}, {2, 4}};
-    vector<int> result = shortestDistanceAfterQueries(5, queries);
-    
-    for (int r : result) {
-        cout << r << " ";
-    }
-    cout << endl;
-    
-    return 0;
-}
-
-    
 };
+
