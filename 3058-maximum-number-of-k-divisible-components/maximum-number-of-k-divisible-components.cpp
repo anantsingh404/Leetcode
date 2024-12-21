3973ad908dc6699c7663dfe0671e4c14e654eecf
Anant Vikram Singh <anantvikrams30@gmail.com>
2024-12-21 11:48:36 +0530

    Time: 94 ms (90.51%) | Memory: 175.3 MB (75.95%) - LeetSync

---
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+vector<int>val;
+vector<vector<int>>adj;
+int kk;
+int count=0;
+
+int solve(int idx,int parent)
+{    int temp=val[idx]%kk;
+    for(auto itr:adj[idx])
+    {
+        if(itr!=parent)
+        {
+            int x=solve(itr,idx);
+            if(x==0)
+            {
+                ++count;
+            }
+            temp+=x;
+        }
+    }
+   
+    return temp%kk;
+
+}
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        val.resize(n);
+        val.assign(values.begin(),values.end());
+        adj.resize(n);
+        kk=k;
+        for(int i=0;i<edges.size();i++)
+        {
+            int x=edges[i][0];
+            int y=edges[i][1];
+            adj[x].push_back(y);
+            adj[y].push_back(x);
+        }
+        solve(0,-1);
+        return count+1;
+
+
+    }
+};
