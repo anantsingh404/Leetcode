358ad4fe58d1c72648d54439604ae1fce0a5dc05
Anant Vikram Singh <anantvikrams30@gmail.com>
2024-12-30 00:46:02 +0530

    Time: 27 ms (84.62%) | Memory: 65 MB (61.54%) - LeetSync

---
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string answerString(string word, int nf) {
+      int n=word.size();
+      int count=n-nf+1;
+      char xx='a';
+      if(nf==1)
+      {
+        return word;
+      }
+      for(int i=0;i<n;i++)
+      {
+        if(word[i]>xx)
+        {
+            xx=word[i];
+        }
+      }
+      int k=0;
+      priority_queue<string>pq;
+      while(k<n)
+      {
+         if(word[k]==xx)
+        {
+        string x=word.substr(k,min(count,n-k));
+         pq.push(x);
+        }
+       
+        k++;
+      }
+      return pq.top();
+
+
+    }
+};
