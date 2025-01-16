de20c4c0e976ffe2f9c16af1e7c8016bbfebc5da
Anant Vikram Singh <anantvikrams30@gmail.com>
2025-01-16 11:56:37 +0530

    Time: 35 ms (7.72%) | Memory: 64.5 MB (12.20%) - LeetSync

---
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+      vector<int>dp1(32,0);
+      vector<int>dp2(32,0);
+      for(int i=0;i<nums1.size();i++)
+      {
+        int j=0;
+        while(j<32)
+        {
+            if(nums1[i]&(1<<j))
+            {
+                dp1[j]++;
+            }
+            j++;
+        }
+      } 
+      for(int i=0;i<nums2.size();i++)
+      {
+        int j=0;
+        while(j<32)
+        {
+            if(nums2[i]&(1<<j))
+            {
+                dp2[j]++;
+            }
+            j++;
+        }
+      }
+      vector<int>dp(32,0);
+      vector<int>zero(32,0);
+      int n=nums1.size();
+      int m=nums2.size();
+      int ans=0;
+      for(int i=0;i<32;i++)
+      {
+        int x=dp1[i];
+        int y=n-dp1[i];
+        int p=dp2[i];
+        int q=m-dp2[i];
+        dp[i]=x*q+p*y;
+        if(dp[i]%2)
+        {
+            ans=ans|(1<<i);
+        }
+       // zero[i]=x*p+y*q;
+      } 
+      return ans;
+
+    }
+};
