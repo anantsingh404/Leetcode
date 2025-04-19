bb46b602dc9d0a2fedac0908ede23ba4a1521685
Anant Vikram Singh <anantvikrams30@gmail.com>
2025-04-19 14:13:37 +0530

    Time: 58 ms (49.83%) | Memory: 60.4 MB (73.14%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-       sort(nums.begin(),nums.end());
-       long long count=0;
-       for(int i=0;i<nums.size();i++)
-       {
-        int x=lower-nums[i];
-        int y=upper-nums[i];
-        
-        int p=lower_bound(nums.begin()+i+1,nums.end(),x)-nums.begin();
-        int q=upper_bound(nums.begin()+i+1,nums.end(),y)-nums.begin();
-       
-        count+=q-p;
-       }
-       return count;
-
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        long long count=0;for(int i=0;i<n-1;i++)
+        {
+            int diff1=lower-nums[i];
+            int diff2=upper-nums[i];
+            int x=lower_bound(nums.begin()+i+1,nums.end(),diff1)-nums.begin();
+            int y=upper_bound(nums.begin()+i+1,nums.end(),diff2)-nums.begin();
+            --y;
+            //cout<<x<<" "<<y<<endl;
+            if(y>=x && x>i)
+            {
+                count+=y-x+1;
+            }
+        }
+        return count;
     }
 };
