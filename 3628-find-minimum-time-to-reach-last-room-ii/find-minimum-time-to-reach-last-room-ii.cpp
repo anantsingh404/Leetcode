a9b7f34871ff7c70f7ea23ed08a4cf3ef2daf197
Anant Vikram Singh <anantvikrams30@gmail.com>
2025-05-08 18:02:15 +0530

    Time: 349 ms (64.98%) | Memory: 106.8 MB (55.27%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-int arr[4]={-1,1,0,0};
-int brr[4]={0,0,-1,1};
-int arr1[4]={-2,2,0,0};
-int brr1[4]={0,0,-2,2};
-    int minTimeToReach(vector<vector<int>>& mt) {
+    int arr[4]={-1,1,0,0};
+    int brr[4]={0,0,-1,1};
+    int minTimeToReach(vector<vector<int>>& mt) 
+    {
       int n=mt.size();
       int m=mt[0].size();
       vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
@@ -16,9 +15,9 @@ int brr1[4]={0,0,-2,2};
         auto itr=pq.top();
         pq.pop();
         int time=itr.first;
+        int turn=itr.second.first;
         int x=itr.second.second.first;
         int y=itr.second.second.second;
-        int turn=itr.second.first;
         if(x==n-1 && y==m-1)
         {
             return time;
@@ -30,7 +29,8 @@ int brr1[4]={0,0,-2,2};
             if(p>=0 && p<n && q>=0 && q<m && turn==1)
             {
                 if(dp[p][q]>time+1)
-                {    int xx=max(mt[p][q]+1,time+1);
+                {   
+                    int xx=max(mt[p][q]+1,time+1);
                     dp[p][q]=xx;
                     pq.push({xx,{2,{p,q}}});
                 }
@@ -38,18 +38,14 @@ int brr1[4]={0,0,-2,2};
             else if(p>=0 && p<n && q>=0 && q<m && turn==2)
             {
                 if(dp[p][q]>time+2)
-                {    int xx=max(mt[p][q]+2,time+2);
+                {   
+                    int xx=max(mt[p][q]+2,time+2);
                     dp[p][q]=xx;
                     pq.push({xx,{1,{p,q}}});
                 }
             }
-            
-         
         }
-
       }
-      return dp[n-1][m-1];
-
-      
+      return dp[n-1][m-1]; 
     }
 };
