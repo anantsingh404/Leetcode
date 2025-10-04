1df1e9f9d9be25969ac917ffb8b4e12ddab951bf
Anant Vikram Singh <anantvikrams30@gmail.com>
2025-10-05 01:02:51 +0530

    Time: 0 ms (100.00%) | Memory: 63.1 MB (12.56%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxArea(vector<int>& h) {
+       int n=h.size();
+       int i=0;
+       int j=n-1;
+       int ans=0;
+       while(i<j)
+       {
+        ans=max(ans,(j-i)*min(h[i],h[j]));
+        if(h[i]<h[j])
+        {
+            ++i;
+        }
+        else
+        {
+            --j;
+        }
+
+       }
+       return ans;
+        
+    }
+};
