ba4566fba34effd86fe8a34ce866ee417bbb125f
Anant Vikram Singh <anantvikrams30@gmail.com>
2025-10-08 16:55:11 +0530

    Time: 1013 ms (5.09%) | Memory: 125.2 MB (65.71%) - LeetSync

---
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& s, vector<int>& p, long long su) {
+       sort(p.begin(),p.end());
+       for(int i=0;i<s.size();i++)
+       {
+        long long x=su/s[i];
+        if(su%s[i]!=0){
+            ++x;
+        }
+        int idx=lower_bound(p.begin(),p.end(),x)-p.begin();
+        cout<<idx<<endl;
+        if(p.size()-idx>0)
+        {
+            s[i]=p.size()-idx;
+        }
+        else
+        {
+            s[i]=0;
+        }
+       }
+       return s;
+        
+    }
+};
