f34d1906af7a6f7628bdea4ace951f4827806321
Anant Vikram Singh <anantvikrams30@gmail.com>
2025-11-21 14:53:29 +0530

    Time: 1469 ms (5.40%) | Memory: 453 MB (5.17%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,29 +1,51 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-         vector<int> R(26,0);
-        for(auto &u : s)
-        {
-            R[u-'a']++;
+        int n=s.size();
+       vector<vector<int>>left(n,vector<int>(26,0));
+       vector<vector<int>>right(n,vector<int>(26,0));  
+      for(int i=0;i<s.size();i++)
+      {
+        int x=s[i]-'a';
+        if(i>0){
+       for(int j=0;j<26;j++)
+       {
+        left[i][j]=left[i-1][j];
+       }
         }
-        vector<int> L(26, 0);
-        
-        unordered_set<int> S;
-        
-        for(int i = 0; i < s.length(); i++)
-        {
-            int t = s[i] - 'a';
-            R[t]--;
-            for(int j = 0; j < 26; j++)
-            {
-                if(L[j] > 0 && R[j] > 0)
-                {
-                    S.insert(26*t + j);
+        ++left[i][x];
+      }
+      for(int i=n-1;i>=0;i--)
+      {
+        int x=s[i]-'a';
+        if(i<n-1){
+       for(int j=0;j<26;j++)
+       {
+        right[i][j]=right[i+1][j];
+       }
+        }
+        ++right[i][x];
+      }
+      
+       unordered_set<string> seen;
+        int ans = 0;
+
+        for(int i = 1; i < n - 1; i++) {
+            for(int j = 0; j < 26; j++) {
+                if(left[i-1][j] > 0 && right[i+1][j] > 0) {
+
+                    string key;
+                    key.push_back(char(j + 'a'));
+                    key.push_back(s[i]);
+                    key.push_back(char(j + 'a'));
+
+                    if(seen.insert(key).second) {
+                        ans++;
+                    }
                 }
             }
-            L[t]++;
         }
-        
-        return S.size(); 
+
+      return ans;
     }
 };
