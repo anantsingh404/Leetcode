c2f9315355313c5e7c05703ada7b5bfd75d5b89c
Anant Vikram Singh <anantvikrams30@gmail.com>
2026-04-23 15:27:46 +0530

    Time: 48 ms (17.94%) | Memory: 129.4 MB (12.11%) - LeetSync

---
+++ 2721-sum-of-distances/sum-of-distances.java
@@ -0,0 +1,44 @@
+class Solution {
+    public long[] distance(int[] nums) {
+        int n = nums.length;
+        long[] dp = new long[n];
+
+        HashMap<Integer, Long> count = new HashMap<>();
+        HashMap<Integer, Long> sum = new HashMap<>();
+
+        int i = 0;
+        while (i < n) {
+            int val = nums[i];
+
+            long c = count.getOrDefault(val, 0L);
+            long s = sum.getOrDefault(val, 0L);
+
+            dp[i] = dp[i] + (c * i - s);
+
+            count.put(val, c + 1);
+            sum.put(val, s + i);
+
+            i++;
+        }
+
+        count.clear();
+        sum.clear();
+
+        i = n - 1;
+        while (i >= 0) {
+            int val = nums[i];
+
+            long c = count.getOrDefault(val, 0L);
+            long s = sum.getOrDefault(val, 0L);
+
+            dp[i] = dp[i] + (s - c * i);
+
+            count.put(val, c + 1);
+            sum.put(val, s + i);
+
+            i--;
+        }
+
+        return dp;
+    }
+}
