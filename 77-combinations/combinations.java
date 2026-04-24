0b5f8647cb1a7e9142a82ab6533bc6174eb5c173
Anant Vikram Singh <anantvikrams30@gmail.com>
2026-04-24 17:52:06 +0530

    Time: 49 ms (5.02%) | Memory: 97.9 MB (67.40%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,30 +1,38 @@
 class Solution {
-    void solve(int idx, int n, int count, List<Integer> temp, List<List<Integer>> ls)
+    void solve(int idx,int dp[],List<List<Integer>>ls,int count,List<Integer>temp)
     {
-        if(count == 0)
+        if(count==0)
         {
             ls.add(new ArrayList<>(temp));
             return;
         }
-
-        if(idx > n)
+        if(idx>=dp.length)
         {
             return;
         }
+        
+            if(dp[idx]==0)
+            {
+                temp.add(idx);
+                dp[idx]=1;
+                solve(idx+1,dp,ls,count-1,temp);
+                dp[idx]=0;
+                temp.remove(temp.get(temp.size()-1));
+            }
+            solve(idx+1,dp,ls,count,temp);
 
-        // take
-        temp.add(idx);
-        solve(idx + 1, n, count - 1, temp, ls);
-        temp.remove(temp.size() - 1);
-
-        // not take
-        solve(idx + 1, n, count, temp, ls);
+        
     }
-
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> ls = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        solve(1, n, k, temp, ls);
-        return ls;
+      List<List<Integer>>ls=new ArrayList<>();
+      ArrayList<Integer>temp=new ArrayList<>();
+      int dp[]=new int[n+1];
+
+      for(int i=0;i<=n;i++)
+      {
+        dp[i]=0;
+      }
+      solve(1,dp,ls,k,temp); 
+      return ls; 
     }
 }
