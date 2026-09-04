50d472a7fdc5bbaf707242b705645fd5dc74367f
Anant Vikram Singh <anantvikrams30@gmail.com>
2026-09-04 15:29:00 +0530

    Time: 4 ms (89.25%) | Memory: 210.9 MB (15.33%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -6,21 +6,25 @@ public:
        vector<int>right(n,INT_MAX);
        left[0]=nums[0];
        right[n-1]=nums[n-1];
+         for(int i=n-2;i>=0;i--)
+       {
+         right[i]=min(right[i+1],nums[i]);
+         
+       }
+       if(left[0]-right[0]<=k)
+       {
+        return 0;
+       }
        for(int i=1;i<n;i++)
        {
          left[i]=max(left[i-1],nums[i]);
-       }
-       for(int i=n-2;i>=0;i--)
-       {
-         right[i]=min(right[i+1],nums[i]);
-       }//int ans=INT_MAX;
-       for(int i=0;i<n;i++)
-       {
-        if(left[i]-right[i]<=k )
+          if(left[i]-right[i]<=k )
         {
             return i;
         }
        }
+     
+     
        return -1;
     }
 };
